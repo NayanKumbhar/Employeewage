c0482cc7667c7b91efadc73461f58a2526474965
NayanKumbhar <nayankumbhar7@gmail.com>
2022-08-03 20:15:26 +0530

    MODIFIED:[Nayan] Check Employee Attedance

--- src/com/kumbhar/Employeewage.java
+++ src/com/kumbhar/Employeewage.java
@@ -5,6 +5,13 @@ public class Employeewage {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to Employee Wage Computation Program");
+		// uc1- check attendance
+		int check_attendance = (int)Math.floor(Math.random()*10)%2;
+		if(check_attendance==1) {
+			System.out.println("Employee is Present");
+		}else {
+			System.out.println("Employee is absent");
+		}
 	}
 
 }
