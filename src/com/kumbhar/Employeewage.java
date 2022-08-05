c77a5854d8accc0152795c90a5b7e6bb60c223fb
NayanKumbhar <nayankumbhar7@gmail.com>
2022-08-05 12:47:42 +0530

    MODIFIED:[Nayan] Check Employeedailywage

--- src/com/kumbhar/Employeewage.java
+++ src/com/kumbhar/Employeewage.java
@@ -2,16 +2,28 @@ package com.kumbhar;
 
 public class Employeewage {
 
+	private static final int WAGE_PER_HR = 20;
+	private static final int FULL_DAY_HR = 8;
+	private static int totalWage = 0;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
 		System.out.println("Welcome to Employee Wage Computation Program");
+		
 		// uc1- check attendance
+		
 		int check_attendance = (int)Math.floor(Math.random()*10)%2;
 		if(check_attendance==1) {
 			System.out.println("Employee is Present");
-		}else {
+			totalWage = WAGE_PER_HR*FULL_DAY_HR;
+			System.out.println("Total Wage per day is=" + totalWage);
+		}else
+		{
 			System.out.println("Employee is absent");
+			System.out.println("total wage="+totalWage);
+
 		}
+		
 	}
 
 }
